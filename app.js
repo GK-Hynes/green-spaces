e71d04303c02bc1ba62b652da93dfc57227e5d9f
Gerard Hynes <gkhynes@gmail.com>
2021-08-03 15:27:08 +0100

    Add ability to delete reviews

--- app.js
+++ app.js
@@ -135,6 +135,18 @@ app.post(
   })
 );
 
+app.delete(
+  "/greenspaces/:id/reviews/:reviewId",
+  catchAsync(async (req, res) => {
+    const { id, reviewId } = req.params;
+    await Greenspace.findByIdAndUpdate(id, {
+      $pull: { reviews: reviewId }
+    });
+    await Review.findByIdAndDelete(reviewId);
+    res.redirect(`/greenspaces/${id}`);
+  })
+);
+
 app.all("*", (req, res, next) => {
   next(new ExpressError("Page not found", 404));
 });
--- models/greenSpace.js
+++ models/greenSpace.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const Review = require("./review");
 const Schema = mongoose.Schema;
 
 const GreenspaceSchema = new Schema({
@@ -13,6 +14,17 @@ const GreenspaceSchema = new Schema({
       ref: "Review"
     }
   ]
+});
+
+// After you delete a Green Space, delete all associated reviews
+GreenspaceSchema.post("findOneAndDelete", async function (doc) {
+  if (doc) {
+    await Review.deleteMany({
+      _id: {
+        $in: doc.reviews
+      }
+    });
+  }
 });
 
 module.exports = mongoose.model("Greenspace", GreenspaceSchema);
