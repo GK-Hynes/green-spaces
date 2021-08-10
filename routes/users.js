b4b79f3252fdcdb9e6cc503af17c1090079b5ed2
Gerard Hynes <gkhynes@gmail.com>
2021-08-10 12:37:48 +0100

    Add registration route logic

--- routes/users.js
+++ routes/users.js
@@ -1,14 +1,26 @@
 const express = require("express");
 const User = require("../models/user");
-
+const catchAsync = require("../utils/catchAsync");
 const router = express.Router();
 
 router.get("/register", (req, res) => {
   res.render("users/register");
 });
 
-router.post("/register", async (req, res) => {
-  res.send(req.body);
-});
+router.post(
+  "/register",
+  catchAsync(async (req, res) => {
+    try {
+      const { email, username, password } = req.body;
+      const user = new User({ email, username });
+      const registeredUser = await User.register(user, password);
+      req.flash("success", "Welcome");
+      res.redirect("/greenspaces");
+    } catch (err) {
+      req.flash("error", err.message);
+      res.redirect("/register");
+    }
+  })
+);
 
 module.exports = router;
