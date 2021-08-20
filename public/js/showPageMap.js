d89cbdb3b4b2070bee08d788355d29a6385180a6
Gerard Hynes <gkhynes@gmail.com>
2021-08-20 16:27:25 +0100

    Add placeholder image and customize map

--- public/js/showPageMap.js
+++ public/js/showPageMap.js
@@ -2,8 +2,15 @@ mapboxgl.accessToken = mapToken;
 console.log(greenspace);
 const map = new mapboxgl.Map({
   container: "map", // container ID
-  style: "mapbox://styles/mapbox/streets-v11", // style URL
+  style: "mapbox://styles/mapbox/light-v10", // style URL
   center: greenspace.geometry.coordinates, // starting position [lng, lat]
   zoom: 9 // starting zoom
 });
-new mapboxgl.Marker().setLngLat(greenspace.geometry.coordinates).addTo(map);
+new mapboxgl.Marker()
+  .setLngLat(greenspace.geometry.coordinates)
+  .setPopup(
+    new mapboxgl.Popup({ offset: 25 }).setHTML(
+      `<h3>${greenspace.title}</h3><p>${greenspace.location}</p>`
+    )
+  )
+  .addTo(map);
