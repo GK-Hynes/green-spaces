7240c264afef8580fd0906e95737799c808b64f7
Gerard Hynes <gkhynes@gmail.com>
2021-08-21 13:52:32 +0100

    Add cluster map

--- models/greenSpace.js
+++ models/greenSpace.js
@@ -7,37 +7,49 @@ const ImageSchema = new Schema({
   filename: String
 });
 
+// Set Cloudinary transforms on image thumbnails
 ImageSchema.virtual("thumbnail").get(function () {
   return this.url.replace("/upload", "/upload/w_200");
 });
 
-const GreenspaceSchema = new Schema({
-  title: String,
-  images: [ImageSchema],
-  geometry: {
-    type: {
-      type: String,
-      enum: ["Point"],
-      required: true
+// Make sure virtuals are kept if Green Space is stringified
+const opts = { toJSON: { virtuals: true } };
+
+const GreenspaceSchema = new Schema(
+  {
+    title: String,
+    images: [ImageSchema],
+    geometry: {
+      type: {
+        type: String,
+        enum: ["Point"],
+        required: true
+      },
+      coordinates: {
+        type: [Number],
+        required: true
+      }
     },
-    coordinates: {
-      type: [Number],
-      required: true
-    }
-  },
-  fee: Number,
-  description: String,
-  location: String,
-  author: {
-    type: Schema.Types.ObjectId,
-    ref: "User"
-  },
-  reviews: [
-    {
+    fee: Number,
+    description: String,
+    location: String,
+    author: {
       type: Schema.Types.ObjectId,
-      ref: "Review"
-    }
-  ]
+      ref: "User"
+    },
+    reviews: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Review"
+      }
+    ]
+  },
+  opts
+);
+
+// Generate content for Green Space cluster map popup
+GreenspaceSchema.virtual("properties.popupMarkup").get(function () {
+  return `<strong><a href="/greenspaces/${this._id}">${this.title}</a></strong>`;
 });
 
 // After you delete a Green Space, delete all associated reviews
--- seeds/index.js
+++ seeds/index.js
@@ -23,7 +23,7 @@ const sample = (array) => array[Math.floor(Math.random() * array.length)];
 
 const seedDB = async () => {
   await Greenspace.deleteMany({});
-  for (let i = 0; i < 20; i++) {
+  for (let i = 0; i < 200; i++) {
     const random1000 = Math.floor(Math.random() * 1000);
     const greenspace = new Greenspace({
       author: "611396aa19e6c10c73234784",
