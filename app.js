d56c99a87b5fdfd80c6d86c0ba3cb601afb66cc7
Gerard Hynes <gkhynes@gmail.com>
2021-08-22 17:07:05 +0100

    Sanitize inputs

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const mongoose = require("mongoose");
 const ejsMate = require("ejs-mate");
 const path = require("path");
 const methodOverride = require("method-override");
+const mongoSanitize = require("express-mongo-sanitize");
 const ExpressError = require("./utils/ExpressError");
 const greenspaceRoutes = require("./routes/greenspaces");
 const reviewRoutes = require("./routes/reviews");
@@ -42,6 +43,7 @@ app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
 app.use(express.static(path.join(__dirname, "public")));
+app.use(mongoSanitize());
 
 // Session
 const sessionConfig = {
